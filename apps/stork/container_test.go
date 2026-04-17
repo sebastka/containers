bfc79ec0fbbf4247175fd791a660afe89928ca8f
Sebastian Karlsen <sebastian@karlsen.fr>
2026-04-17 20:38:46 +0200

    fix: Container test command

--- apps/stork/container_test.go
+++ apps/stork/container_test.go
@@ -10,5 +10,5 @@ import (
 func Test(t *testing.T) {
 	ctx := context.Background()
 	image := testhelpers.GetTestImage("ghcr.io/sebastka/stork:rolling")
-	testhelpers.TestCommandSucceeds(t, ctx, image, nil, "/stork-server", "--version")
+	testhelpers.TestCommandSucceeds(t, ctx, image, nil, "stork-server", "--version")
 }
